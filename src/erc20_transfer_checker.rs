eb40e657629d87c14098fc6decf63e3572536e62
xBalbinus <xiangan9@gmail.com>
2024-01-30 21:51:32 -0500

    feat: maximum allowance (take any amount of token at any time) check

--- src/erc20_transfer_checker.rs
+++ src/erc20_transfer_checker.rs
@@ -42,11 +42,43 @@ impl EvmTransactionChecker for Erc20TransferChecker {
             result.warnings.push(warning);
         }
 
+        if let Some(warning) = check_erc20_maximum_allowance(data)? {
+            result.warnings.push(warning);
+        }
+
         Ok(result)
     }
 }
 
-/// If the method call is a `Transfer` and the destination address is identified as a contract address
+/// If the method call is an `Approve` and the approved amount is maximum (2^256 - 1),
+/// it generates a warning indicating that the spender can withdraw any amount at any time.
+fn check_erc20_maximum_allowance(data: &str) -> anyhow::Result<Option<String>> {
+    let data_bytes = hex::decode(data)
+        .context("Failed to decode transaction data from hexadecimal")?;
+
+    // Assume for now that empty data objects (eth_send) are not ERC20 transfers and therefore don't endanger ERC assets
+    if data_bytes.len() < 4 {
+        return Ok(None);
+    }
+
+    let method = &data_bytes[0..4];
+    let method_vec = method.to_vec();
+    let erc20_method = ERC20Method::from(method_vec);
+
+    if erc20_method == ERC20Method::Approve && data_bytes.len() >= 36 {
+        let approved_amount = &data_bytes[36..68];
+        let max_allowance = [255u8; 32];
+
+        if approved_amount == &max_allowance[..] {
+            let warning = "ERC20 maximum allowance given. The spender can withdraw any amount at any time.".to_string();
+            return Ok(Some(warning));
+        }
+    }
+
+    Ok(None)
+}
+
+/// If the method call is a `Transfer` and the destination address is identified as a token contract address
 /// (not an EOA), it generates a warning indicating tokens may be lost forever (e.g. in contracts like WETH)
 fn check_erc20_transfer_to_token_contract(data: &str, to_address: &str) -> anyhow::Result<Option<String>> {
     let to_address_bytes = H160::from_str(to_address)
@@ -63,18 +95,20 @@ fn check_erc20_transfer_to_token_contract(data: &str, to_address: &str) -> anyho
     let method = &data_bytes[0..4];
     let method_vec = method.to_vec();
     let erc20_method = ERC20Method::from(method_vec);
-    let contract_address = ContractAddress::from(to_address_bytes);
+    let token_contract_address = ContractAddress::from(to_address_bytes);
 
-    if erc20_method == ERC20Method::Transfer && contract_address != ContractAddress::Unidentified(to_address_bytes) {
+    if erc20_method == ERC20Method::Transfer && token_contract_address != ContractAddress::Unidentified(to_address_bytes) {
         let warning = format!(
-            "ERC20 transfer to contract address {:?}",
-            contract_address
+            "ERC20 transfer to token contract address {:?}",
+            token_contract_address
         );
         return Ok(Some(warning));
     }
 
     Ok(None)
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -118,6 +152,26 @@ mod tests {
         let checker = Erc20TransferChecker {};
         let result = checker.run(&context).await?;
 
+        assert!(!result.warnings.is_empty(), "warning on ERC20 transfer");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn it_should_warn_if_max_allowance_given() -> anyhow::Result<()> {
+        let transaction = EvmTransactionObject {
+            from: Some("0x6b175474e89094c44da98b954eedeac495271d0f".to_string()),
+            to: Some("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48".to_string()),
+            data: Some("0x095ea7b30000000000000000000000006b175474e89094c44da98b954eedeac495271d0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff".to_string()),
+            value: Some("0".to_string()),
+            gas: None,
+        };
+
+        let context = EvmTransactionCheckerContext {
+            transaction: &transaction,
+        };
+        let checker = Erc20TransferChecker {};
+        let result = checker.run(&context).await?;
+        
         assert!(!result.warnings.is_empty(), "warning on ERC20 transfer");
         Ok(())
     }
